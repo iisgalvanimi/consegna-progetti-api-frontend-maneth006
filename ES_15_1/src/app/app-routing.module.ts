3ab91884d91dcb80a02cbab4558d4143206e5422
maneth wijeratne <wijeratne.maneth@iisgalvanimi.edu.it>
2024-11-18 13:43:09 +0000

    commit finale 15.1

---
+++ ES_15_1/src/app/app-routing.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { FooComponent } from './foo/foo.component';
+const routes: Routes = [
+  { path: 'foo', component: FooComponent},
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
